4ad229381a01d4a3dd92f8ff225372a39e59c7f0
Ilya Gorenburg <ilya.gorenburg@gmail.com>
2024-12-27 14:05:11 -0500

    js: method update

--- src/lib/functions/base_path.ts
+++ src/lib/functions/base_path.ts
@@ -2,7 +2,7 @@ import { base } from '$app/paths'
 
 export function getPreviewImageUrl(path: string): string {
   if (path.includes('/images/')) {
-    return base + '/' + path
+    return base + path
   }
 
   return path
