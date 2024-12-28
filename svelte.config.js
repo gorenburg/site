628a46615027fcfa9017de8804c894251645426c
Ilya Gorenburg <ilya.gorenburg@gmail.com>
2024-12-27 22:33:58 -0500

    js: updated 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -37,7 +37,7 @@ const config = {
       pages: 'build',
       assets: 'build',
       strict: false,
-      fallback: '404.html'
+      fallback: 'index.html'
     }),
     prerender: {
       entries: [
