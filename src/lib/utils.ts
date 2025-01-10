45d07e427e193b9b60f539a11b09234fa47349d2
Ilya Gorenburg <ilya.gorenburg@gmail.com>
2025-01-09 19:57:26 -0500

    js: properly format time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 type DateStyle = Intl.DateTimeFormatOptions['dateStyle']
 
-export function formatDate(date: string, dateStyle: DateStyle = 'medium', locales = 'en'): string {
-	const dateToFormat = new Date(date)
-	const dateFormatter = new Intl.DateTimeFormat(locales, { dateStyle })
-	return dateFormatter.format(dateToFormat)
+export function formatDate(date: string, dateStyle: DateStyle = 'long', locales = 'en-US') {
+  const dateToFormat = new Date(date.replace(/ /, 'T').replace(/ /, ''))
+  return new Intl.DateTimeFormat(locales, { dateStyle }).format(dateToFormat)
 }
